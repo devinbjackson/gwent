90914c5b9a626007dbe00046bf75f2074551f61a
Devin B Jackson <mrdevidu@gmail.com>
2018-03-02 20:31:06 -0600

    feat: add card

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+            I'm a card!
+      </div>
+    );
+  }
+}
+
+export default Card;
